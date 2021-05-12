3edce1a60c2899400dc95fd6c38d2dd891e66254
Jingu Kang <jinguk0816@gmail.com>
2021-05-11 23:35:07 -0400

    change Appointment component so it renders Header, Show and Empty component depending on the state of the appointment either booked or not

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment }from "react";
 
 import { addParameters, storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -18,6 +18,7 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+
 
 
 storiesOf("Button", module)
@@ -173,3 +174,13 @@ storiesOf("Appointment", module)
       onSave={action("onSave")} 
       onCancel={action("onCancel")}
     />)
+.add("Appointment Booked", () => (
+  <Fragment>
+    <Appointment
+      id={1}
+      time="12pm"
+      interview={{ student: "Lydia Miller-Jones", interviewer }}
+    />
+    <Appointment id="last" time="1pm" />
+  </Fragment>
+))
