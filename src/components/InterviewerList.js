13b222f274ae6e89db75fa4a58638496cd05a5e0
Jingu Kang <jinguk0816@gmail.com>
2021-05-11 22:19:28 -0400

    implement InterviewerList component with the css style of the component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterviwerList(props) {
+  const parsedInterviewerList = props.interviewers.map(interviewer => 
+  <InterviewerListItem 
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.interviewer} 
+    setInterviewer={event => props.setInterviewer(interviewer.id)}
+  />);
+
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewerList}</ul>
+    </section>
+  );
+}
