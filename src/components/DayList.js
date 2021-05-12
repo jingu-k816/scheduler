305f2284d05dae59af8c537b035c06216d71124c
Jingu Kang <jinguk0816@gmail.com>
2021-05-11 22:20:21 -0400

    Implement Single InterviewerListItem component and add styles to the component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,8 +2,13 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const days = props.days.map(day => <DayListItem key={day.id} selected={day.name === props.day}
-    setDay={props.setDay} {...day} />);
+  const days = props.days.map(day => 
+  <DayListItem 
+    key={day.id} 
+    selected={day.name === props.day}
+    setDay={props.setDay} 
+    {...day} 
+  />);
   
   return (
     <ul>
