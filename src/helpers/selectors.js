04429dd9d21cad9d368eb66398e594d84a68c1c1
Jingu Kang <jinguk0816@gmail.com>
2021-05-13 19:41:00 -0400

    make getInterview function to return null if there is no interview and return the object when there is an interview also test using Jest before deployment

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,3 +9,12 @@ export function getAppointmentsForDay(state, day) {
   return filteredDay.length === 0 ? [] 
   : filteredDay[0].appointments.map(appointment => state.appointments[appointment]);
 }
+
+export function getInterview(state, interview) {
+  return !interview 
+  ? null 
+  : {
+      ...interview, // only uses {student: } part of the object since interviewer is being used in the following line
+      "interviewer" : state.interviewers[interview.interviewer]
+    };
+}
