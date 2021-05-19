6b07d63544d6b6cb958ea17d59ec9a66d2dbe4a8
Jingu Kang <jinguk0816@gmail.com>
2021-05-19 10:01:25 -0400

    add axios delete for integration test

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -81,10 +81,18 @@ export default {
     }
   }),
 
-  put: jest.fn(url => {
+  put: jest.fn( () => {
+    return Promise.resolve({
+      status: 200,
+      statusText: "No Content"
+    })
+  }),
+
+  delete: jest.fn( () => {
     return Promise.resolve({
       status: 200,
       statusText: "No Content"
     })
   })
+
 }
